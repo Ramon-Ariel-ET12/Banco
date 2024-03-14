83be41b131faccac4aa2b0e8106f1a71d7cbe839
frank321312 <hectorsacaca1123@gmail.com>
2024-03-14 15:26:54 -0300

    Updated

--- Biblioteca/Banco.cs
+++ Biblioteca/Banco.cs
@@ -4,19 +4,4 @@ public class Banco
 {
     public List<Cliente> clientes { get; set; }
     public Banco () =>  clientes = new List<Cliente>();
-
-    #region 'registrarCliente'
-    public void RegistrarCliente(Cliente cliente)
-    {
-        foreach (var i in clientes)
-        {
-            if (i.Dni != cliente.Dni)
-            {
-                throw new InvalidOperationException("el DNI " + cliente.Dni + " ya está en uso");
-            }
-        }
-        clientes.Add(cliente);
-    }
-    #endregion
-
 }
